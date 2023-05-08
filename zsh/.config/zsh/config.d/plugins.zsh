86f9af222721cc61b12d5c03e15b98f64fc7fb3b
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-05-08 10:19:26 -0500

    Hook asdf

--- zsh/.config/zsh/config.d/plugins.zsh
+++ zsh/.config/zsh/config.d/plugins.zsh
@@ -12,6 +12,7 @@ znap source zsh-users/zsh-autosuggestions
 
 znap source MichaelAquilina/zsh-you-should-use
 
+. $(brew --prefix asdf)/libexec/asdf.sh
 znap eval direnv 'direnv hook zsh'
 znap eval zsh-completions "znap install zsh-users/zsh-completions"
 znap eval iterm2 'curl -fsSL https://iterm2.com/shell_integration/zsh'
