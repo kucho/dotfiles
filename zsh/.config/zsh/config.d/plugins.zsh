8b7bbd253cd3330e59d5a330f17c7baff72d7150
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-05-25 14:18:31 -0500

    Add gcloud autocomplete

--- zsh/.config/zsh/config.d/plugins.zsh
+++ zsh/.config/zsh/config.d/plugins.zsh
@@ -11,6 +11,7 @@ ZSH_AUTOSUGGEST_STRATEGY=(history)
 znap source zsh-users/zsh-autosuggestions
 
 znap source MichaelAquilina/zsh-you-should-use
+znap source wintermi/zsh-gcloud
 
 znap eval zsh-completions "znap install zsh-users/zsh-completions"
 znap fpath _kubectl 'kubectl completion zsh'
