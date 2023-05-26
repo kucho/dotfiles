8d40d7b42caaa8cfcd4d8536a8526ddd79e7639a
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-05-25 21:40:03 -0500

    Add env zsh config

---
+++ zsh/.config/zsh/config.d/03-env.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+##
+# Environment variables
+#
+
+# -U ensures each entry in these is unique (that is, discards duplicates).
+export -U PATH path FPATH fpath MANPATH manpath
+export -UT INFOPATH infopath # -T creates a "tied" pair; see below.
+
+# $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
+# Modifying one will also modify the other.
+# Note that each value in an array is expanded separately. Thus, we can use ~
+# for $HOME in each $path entry.
+path=(
+  $path
+  ~/.local/bin
+)
+
+# Add your functions to your $fpath, so you can autoload them.
+fpath=(
+  $ZDOTDIR/functions
+  $fpath
+  ~/.local/share/zsh/site-functions
+)
