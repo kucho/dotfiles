b36440070a53ef524efaa706b4e94b13ebbfa9ce
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-05-25 22:03:25 -0500

    Add commands config file

---
+++ zsh/.config/zsh/config.d/99-commands.zsh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+
+##
+# Commands, funtions and aliases
+#
+# Always set aliases _last,_ so they don't get used in function definitions.
+#
+
+# This lets you change to any dir without having to type `cd`, that is, by just
+# typing its name. Be warned, though: This can misfire if there exists an alias,
+# function, builtin or command with the same name.
+# In general, I would recommend you use only the following without `cd`:
+#   ..  to go one dir up
+#   ~   to go to your home dir
+#   ~-2 to go to the 2nd mostly recently visited dir
+#   /   to go to the root dir
+setopt AUTO_CD
+
+# Type '-' to return to your previous dir.
+alias -- -='cd -'
+# '--' signifies the end of options. Otherwise, '-=...' would be interpreted as
+# a flag.
+
+# These aliases enable us to paste example code into the terminal without the
+# shell complaining about the pasted prompt symbol.
+alias %= \$=
+
+# Set $PAGER if it hasn't been set yet. We need it below.
+# `:` is a builtin command that does nothing. We use it here to stop Zsh from
+# evaluating the value of our $expansion as a command.
+: ${PAGER:=less}
+
+# Associate file name .extensions with programs to open them.
+# This lets you open a file just by typing its name and pressing enter.
+# Note that the dot is implicit; `gz` below stands for files ending in .gz
+alias -s {css,gradle,html,js,json,md,patch,properties,txt,xml,yml}=$PAGER
+alias -s gz='gzip -l'
+alias -s {log,out}='tail -F'
+
+# Use `< file` to quickly view the contents of any text file.
+READNULLCMD=$PAGER # Set the program to use for this.
+
+alias lg='lazygit'
+alias dev='bin/dev'
+
+# Get my ip
+function myip {
+  dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"'
+}
+
+# Check ports
+function listening {
+  if [ $# -eq 0 ]; then
+    sudo lsof -iTCP -sTCP:LISTEN -n -P
+  elif [ $# -eq 1 ]; then
+    sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color $1
+  else
+    echo "Usage: listening [pattern]"
+  fi
+}
