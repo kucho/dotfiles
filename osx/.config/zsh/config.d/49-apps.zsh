bc9bf2e0120308dcb48725508e76f49eb260a47f
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-05-25 23:09:31 -0500

    Improve osx zsh config

--- osx/.config/zsh/config.d/49-apps.zsh
+++ osx/.config/zsh/config.d/49-apps.zsh
@@ -1,13 +1,32 @@
+#!/bin/zsh
+
+##
+# OS specific dependencies
+#
+
+# Insall brew and dependencies
 function install_homebrew {
   if [[ $(command -v brew) == "" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>/dev/null
   else
     brew update &>/dev/null
   fi
+
+  cd ~ && brew bundle &>/dev/nul
 }
 
 znap eval _homebrew install_homebrew
-znap eval _install_dependencies 'cd ~ && brew bundle &>/dev/null'
+
+# Add libpq to path
+path+=($(brew --prefix libpq)/bin)
+
+# Enable asdf
 . $(brew --prefix asdf)/libexec/asdf.sh
+
+# Hook zsh to shell
 znap eval direnv 'direnv hook zsh'
+
 znap eval iterm2 'curl -fsSL https://iterm2.com/shell_integration/zsh'
+
+# Use 1Password auth sock
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
--- osx/.config/zsh/config.d/exports.zsh
+++
@@ -1,4 +0,0 @@
-export GPG_TTY=$(tty)
-export PATH="$(brew --prefix libpq)/bin:$PATH"
-export ERL_AFLAGS="-kernel shell_history enabled"
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
