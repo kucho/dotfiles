83ebfc04f28374d4e545cf031cd8683160d75c49
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2023-11-15 16:12:20 -0500

    Add ruby opts

--- zsh/.config/zsh/config.d/30-env.zsh
+++ zsh/.config/zsh/config.d/30-env.zsh
@@ -8,6 +8,10 @@
 export -U PATH path FPATH fpath MANPATH manpath
 export -UT INFOPATH infopath # -T creates a "tied" pair; see below.
 
+# Ruby opts
+export RUBY_CONFIGURE_OPTS=--enable-yjit
+export RUBY_YJIT_ENABLE=1
+
 # $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
 # Modifying one will also modify the other.
 # Note that each value in an array is expanded separately. Thus, we can use ~
