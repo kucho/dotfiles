de06922c863c09514ec18f26501e441875720300
Victor Rodriguez <kuchox@pm.me>
2023-12-11 10:15:04 -0500

    Add fix for apple chips

--- osx/.config/zsh/config.d/30-dependencies.zsh
+++ osx/.config/zsh/config.d/30-dependencies.zsh
@@ -4,6 +4,12 @@
 # OS specific dependencies
 #
 
+# Use 1Password auth sock
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+# Issue: https://github.com/rails/rails/issues/38560
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 # Insall brew and dependencies
 if (( ! ${+commands[brew]} )); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>/dev/null
@@ -14,9 +20,6 @@ fpath+=($(brew --prefix)/share/zsh/site-functions)
 
 # Add libpq to path
 path+=($(brew --prefix libpq)/bin)
-
-# Use 1Password auth sock
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 if (( ! ${+commands[rtx]} )); then
   curl https://rtx.pub/rtx-latest-macos-arm64 > ~/.local/bin/rtx
