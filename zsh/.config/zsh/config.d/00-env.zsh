0ac71b86a1dcc25d921a0f1fe2ee0dad4e1439ac
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2024-09-28 22:15:27 -0500

    refactor: clean up zsh config

--- zsh/.config/zsh/config.d/00-env.zsh
+++ zsh/.config/zsh/config.d/00-env.zsh
@@ -4,44 +4,16 @@
 # Environment variables
 #
 
-# Issue https://github.com/zimfw/zimfw/issues/504
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-
-export ZSH_CACHE_DIR=${XDG_CACHE_HOME:-$HOME/.cache}/zsh
-mkdir -p $ZSH_CACHE_DIR/{history,fc-cache,completions}
-
-# Highlight matches in autosuggestions
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=137'
-
-# -U ensures each entry in these is unique (that is, discards duplicates).
-export -U PATH path FPATH fpath MANPATH manpath
-export -UT INFOPATH infopath # -T creates a "tied" pair; see below.
-
 # Ruby opts
 export RUBY_CONFIGURE_OPTS=--enable-yjit
 export RUBY_YJIT_ENABLE=1
 
 export BUN_INSTALL="$HOME/.bun"
 
-# $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
-# Modifying one will also modify the other.
-# Note that each value in an array is expanded separately. Thus, we can use ~
-# for $HOME in each $path entry.
-
-mkdir -p ~/.local/bin
+[ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
 
 path=(
   $path
   ~/.local/bin
   $BUN_INSTALL/bin
-)
-
-# Add your functions to your $fpath, so you can autoload them.
-fpath=(
-  $ZDOTDIR/functions
-  $fpath
-  ~/.local/share/zsh/site-functions
 )
--- zsh/.config/zsh/config.d/40-plugins.zsh
+++ zsh/.config/zsh/config.d/40-plugins.zsh
@@ -3,17 +3,12 @@
 ZIM_HOME=~/Git/zim
 # Download zimfw plugin manager if missing.
 if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  if (( ${+commands[curl]} )); then
-    curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  else
-    mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
-        https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-  fi
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 fi
 
-# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+# Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
   source ${ZIM_HOME}/zimfw.zsh init -q
 fi
 
@@ -23,13 +18,6 @@ source ${ZIM_HOME}/init.zsh
 if (( ! ${+commands[mise]} )); then
   curl https://mise.jdx.dev/install.sh | sh
 fi
-
-if [ ! -f ~/Git/fzf/install ]; then
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/Git/fzf
-	printf 'y\ny\ny\n' | ~/Git/fzf/install
-fi
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 if (( ! ${+commands[cargo]} )); then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile default
