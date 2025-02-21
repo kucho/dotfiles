8131c60d99230af090184c2a98f13f0ef96e83f3
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2025-02-21 09:37:55 -0500

    Remove YJIT bits

--- zsh/.config/zsh/config.d/00-env.zsh
+++ zsh/.config/zsh/config.d/00-env.zsh
@@ -5,11 +5,6 @@
 #
 
 export MISE_ENV_FILE=.env
-
-# Ruby opts
-export RUBY_CONFIGURE_OPTS=--enable-yjit
-export RUBY_YJIT_ENABLE=1
-
 export BUN_INSTALL="$HOME/.bun"
 
 [ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
