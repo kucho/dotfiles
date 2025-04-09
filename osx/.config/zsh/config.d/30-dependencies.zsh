a512b2a22d9b278cde633318f04c32bd90213530
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2025-04-09 08:57:34 -0500

    Remove OBJC_DISABLE_INITIALIZE_FORK_SAFETY export

--- osx/.config/zsh/config.d/30-dependencies.zsh
+++ osx/.config/zsh/config.d/30-dependencies.zsh
@@ -7,9 +7,6 @@
 # Use 1Password auth sock
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
-# Issue: https://github.com/rails/rails/issues/38560
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
 # Insall brew and dependencies
 if (( ! ${+commands[brew]} )); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>/dev/null
