6ed0e1b19e27ed629d10fabf9b2a2a793a08233c
Victor Rodriguez <kuchox@pm.me>
2026-05-27 15:44:32 -0500

    Add fisher

--- fish/.config/fish/conf.d/40-plugins.fish
+++ fish/.config/fish/conf.d/40-plugins.fish
@@ -10,5 +10,8 @@ else
     mise activate fish --shims | source
 end
 
+if not type -q fisher
+  curl -sL https://git.io/fisher | source
+end
+
 starship init fish | source
-zoxide init fish | source
